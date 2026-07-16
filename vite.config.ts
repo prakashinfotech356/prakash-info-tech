b4abfe1b38a602b44c3887741d737d3060efd923
prakashinfotech356 <prakashinfotech356@gmail.com>
2026-07-16 09:01:37 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -20,3 +20,10 @@ export default defineConfig(() => {
     },
   };
 });
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+export default defineConfig({
+  plugins: [react()],
+  base: '/YOUR_REPOSITORY_NAME/',
+})
